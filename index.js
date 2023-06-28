6e6c9b950f0374e9b36de576ff86888022c8104c
Raghav <raghav.g@zebpay.com>
2023-06-28 16:35:35 +0530

    liminal wallets balance tracking script

--- index.js
+++ index.js
@@ -1,1 +1,49 @@
 require('dotenv').config()
+let liminalContracts= require("./liminal.contracts")
+
+let liminal= require ("@lmnl/liminaljs")
+
+let liminalConfig={
+    clientId:process.env.LIMINALCONFIG_CLIENTID,
+    clientSecret: process.env.LIMINALCONFIG_CLIENTSECRET
+}
+let liminalJs = null;
+var liminalAuthenticate = false;
+
+// walletId should be number
+var walletId=778
+
+let token="XLM"
+
+let parentChain="XLM"
+
+async function helper(){
+    // console.log(liminalConfig)
+    if (!liminalAuthenticate) {
+        liminalJs = new liminal.LiminalJs(liminal.LiminalEnvironment.prod);
+        await liminalJs.Authenticate({ clientId: liminalConfig.clientId, clientSecret: liminalConfig.clientSecret }).AuthenticateWithAccessToken();
+        liminalAuthenticate = true;
+        console.log("Liminal client authenticated")
+    }
+    let walletData
+    if(parentChain === token){
+        walletInstance = await liminalJs.Coin(liminal.CoinsEnum[token.toLowerCase()]).Wallets().Get({ walletId })
+         walletData = await walletInstance.GetWallet();
+    }else{
+        let tokenContractAddress = liminalContracts.hasOwnProperty(parentChain.toUpperCase())&& liminalContracts[parentChain.toUpperCase()].hasOwnProperty(token.toUpperCase())? liminalContracts[parentChain.toUpperCase()][token.toUpperCase()].address: null
+        if(tokenContractAddress == undefined || tokenContractAddress == null||tokenContractAddress.length==0){
+            throw new Error(`${token} contract address not found for chain ${parentChain}`)
+        }
+        walletInstance=await liminalJs.Coin(liminal.CoinsEnum[parentChain]).Token({tokenAddress:tokenContractAddress,tokenName:token}).Wallets().Get({walletId});
+         walletData = await walletInstance.GetWallet();
+    }
+    console.log(walletData.Data)
+}
+// function helper2(){
+//     console.log(liminalContracts.BNB)
+//         console.log(liminalContracts[token.toUpperCase()])
+// }
+
+// helper2()
+
+helper()
---
+++ liminal.contracts.js
@@ -0,0 +1,66 @@
+module.exports =liminalContracts = {
+    'ETH': {
+        'MKR': {
+            name: 'Maker',
+            divider: 1000000000000000000,
+            symbol: 'MKR',
+            minUnit: 'wei',
+            ourUnit: 'szabo',
+            address: '0x9f8F72aA9304c8B593d555F12eF6589cC3A579A2',
+            COLD_STORAGE: '0x9f8F72aA9304c8B593d555F12eF6589cC3A579A2',
+            TOKENMinimumBalance: '2',
+        },
+        'BAT': {
+            name: 'Basic Attention TOken',
+            divider: 1000000000000000000,
+            symbol: 'BAT',
+            minUnit: 'wei',
+            ourUnit: 'szabo',
+            address: '0x0D8775F648430679A709E98d2b0Cb6250d2887EF',
+            COLD_STORAGE: '0x0D8775F648430679A709E98d2b0Cb6250d2887EF',
+            TOKENMinimumBalance: '2',
+        },
+        'USDT': {
+            name: 'Tether USD',
+            divider: 1000000,
+            symbol: 'USDT',
+            minUnit: 'wei',
+            ourUnit: 'szabo',
+            address: '0xdAC17F958D2ee523a2206206994597C13D831ec7',
+            COLD_STORAGE: '0xdAC17F958D2ee523a2206206994597C13D831ec7',
+            TOKENMinimumBalance: '2',
+        },
+        'PAXG': {
+            name: 'PAXG',
+            divider: 1000000000000000000,
+            symbol: 'PAXG',
+            minUnit: 'wei',
+            ourUnit: 'szabo',
+            address: '0x45804880de22913dafe09f4980848ece6ecbaf78',
+            COLD_STORAGE: '0xd7fe554b1d08d6ee9fad927c89c9546077462f16',
+            TOKENMinimumBalance: '0.000001'
+        },
+    },
+    'BNB': {
+        'INJ': {
+            name: 'Injective Protocol',
+            divider: 1000000000000000000,
+            symbol: 'INJ',
+            minUnit: 'wei',
+            ourUnit: 'szabo',
+            address: '0xa2B726B1145A4773F68593CF171187d8EBe4d495',
+            COLD_STORAGE: '0xa2B726B1145A4773F68593CF171187d8EBe4d495',
+            TOKENMinimumBalance: '2',
+        },
+        'BTT': {
+            name: 'BitTorrent',
+            divider: 1000000000000000000,
+            symbol: 'BTT',
+            minUnit: 'wei',
+            ourUnit: 'szabo',
+            address: '0x352Cb5E19b12FC216548a2677bD0fce83BaE434B',
+            COLD_STORAGE: '0x352Cb5E19b12FC216548a2677bD0fce83BaE434B',
+            TOKENMinimumBalance: '2',
+        }
+    }
+}
